bdb6d25a5b32fbe192ea2c764a5d105a9644f030
Rick Nitsche <rick@phas.ubc.ca>
2020-07-16 14:00:49 -0700

    docs(opinion): add classes overview

--- chimedb/dataflag/__init__.py
+++ chimedb/dataflag/__init__.py
@@ -1,3 +1,41 @@
+"""
+CHIME Data Flag and Opinion - Database Package
+
+Opinion Database Model Classes
+==============================
+
+* DataFlagClient
+    The client (software) used to create a dataflag. This could for example be
+    `chimedb.dataflag.client.py` or `bondia`. The client is described by a name and version.
+
+* DataFlagOpinionType
+    A type for dataflag opinions. This should explain how they are created: manually, through a
+    web interface (like bondia), etc.
+
+* DataFlagOpinion
+    Instead of directly creating data flags and adding them to the database, users can add their
+    opinions on flagging subsets of data. User opinions can later get translated to flags.
+
+* DataFlagVote
+    A vote is the action that translates existing user opinions to data flags. The voting mode name
+    is stored here. Voting modes are implemented in `VotingJudge`.
+
+* DataFlagVoteOpinion
+    Many-to-many relationship between votes and opinions.
+
+* DataRevision
+    The revision of the offline pipeline that created the data that opinions and votes are based
+    on. An opinion is always strictly based on the revision of the offline pipeline that produced
+    the data the user looked at to make a decision. A vote is still based on a revision, but may
+    include opinions from other revisions (depending on the voting mode).
+
+Voting
+======
+
+* VotingJudge
+    Can translate from opinions to flags when run.
+"""
+
 # === Start Python 2/3 compatibility
 from __future__ import absolute_import, division, print_function, unicode_literals
 from future.builtins import *  # noqa  pylint: disable=W0401, W0614
